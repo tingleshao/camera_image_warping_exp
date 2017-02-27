9243440c56a410220d037eb1ced8f0004bc2d0e0
tingleshao <cshao@cs.unc.edu>
2017-02-27 15:56:00 -0500

    remove the long comments inthe third warping script.

---
+++ rotation_iterator.m
@@ -0,0 +1,9 @@
+% the goal of this script is to find the correct pitch and yaw angles so
+% that the images are translated to the given corner positions 
+ 
+
+function rotation_iterator(corner, image, starting_euls): 
+
+
+
+end
