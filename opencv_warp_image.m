74332eb791c79c01801dced906dc2e691ce8b369
tingleshao <cshao@cs.unc.edu>
2017-03-03 15:09:21 -0500

    update warp_image function

--- opencv_warp_image.m
+++ opencv_warp_image.m
@@ -3,18 +3,24 @@ function [ output_image, corner ] = opencv_warp_image( input_image, R, K )
 % example. 
 
 % spherical mapping
+%{
+C++ code...
 
-  UMat xmap, ymap;
-    Rect dst_roi = buildMaps(src.size(), K, R, xmap, ymap);
+UMat xmap, ymap;
+Rect dst_roi = buildMaps(src.size(), K, R, xmap, ymap);
+dst.create(dst_roi.height + 1, dst_roi.width + 1, src.type());
+remap(src, dst, xmap, ymap, interp_mode, border_mode);
 
-    dst.create(dst_roi.height + 1, dst_roi.width + 1, src.type());
-    remap(src, dst, xmap, ymap, interp_mode, border_mode);
+return dst_roi.tl();
+%}
 
-    return dst_roi.tl();
-    
-    
-    
+% build maps 
+[xmap, ymap, dst] = opencv_build_maps(size(image), K, R);
 
+result = opencv_remap(input_image, xmap, ymap);
+
+corner = dst;
+output_image = result;
 end
 
 
